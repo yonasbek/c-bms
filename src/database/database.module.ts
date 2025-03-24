8b639372901bf24c8ed2896110eaee9f0f323f18
Yonas Bekele <yonasbek4@gmail.com>
2025-03-24 09:33:03 +0300

    db converted to env

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'cbms',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true, // Auto-migrates schema
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
